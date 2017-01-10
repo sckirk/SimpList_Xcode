f922e45173010d0d3d55a80fa3939345acec9909
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2017-01-09 22:29:00 -0800

    ADD A NEW ITEM view: wrote the code so that typing a value into the text field saves it as the text of the label.

--- SimpList/ViewController.swift
+++ SimpList/ViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var itemNameLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        itemNameLabel.text = textField.text
+    }
+    
+    
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    //MARK: Actions
 
 
 }
